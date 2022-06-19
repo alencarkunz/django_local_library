2c7eebb2b58001d9181efe9b4597e1d754a00842
alencarkunz <alenkar.k@gmail.com>
2022-06-19 16:25:08 -0300

    commit

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -79,14 +79,14 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-""" """
+""" 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
 """
 DATABASES = {  
     'default': {  
@@ -99,7 +99,7 @@ DATABASES = {
     }  
 }  
 """
-"""
+""" """
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -110,7 +110,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
