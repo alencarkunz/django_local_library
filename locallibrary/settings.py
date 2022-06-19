2445df598b057b81a86721a6dc06cf628a3d556c
alencarkunz <alenkar.k@gmail.com>
2022-06-19 16:16:45 -0300

    commit 7

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -145,6 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 #STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
 # Default primary key field type
